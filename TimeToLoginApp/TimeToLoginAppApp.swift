d2824a2aa782eb0c9d56ccac2ed044d5637bfdb5
Yurii <yourussianbrother@gmail.com>
2022-12-17 22:00:53 +0300

    Initial Commit

---
+++ TimeToLoginApp/TimeToLoginAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeToLoginAppApp.swift
+//  TimeToLoginApp
+//
+//  Created by Jorgen Shiller on 17.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct TimeToLoginAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
